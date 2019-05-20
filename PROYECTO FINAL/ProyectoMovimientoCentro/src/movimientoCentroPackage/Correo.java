24d4196ddcf92ea0408a07f39159c40fd0b3b036
unknown <pcostainf@gmail.com>
2019-05-20 20:37:08 +0200

    Actualizacion

--- PROYECTO FINAL/ProyectoMovimientoCentro/src/movimientoCentroPackage/Correo.java
+++ PROYECTO FINAL/ProyectoMovimientoCentro/src/movimientoCentroPackage/Correo.java
@@ -58,9 +58,10 @@ public String correoEvento(Voluntario voluntario, Evento evento) {
 		
 		String plantilla = "<h1>Hola de nuevo "+voluntario.getNombre()+"</h1>\r\n" + 
 				"		        \r\n" + 
-				"		        	<img src=\"https://am-tm.es/wp-content/uploads/2019/04/eg-2019.png\">\r\n" + 
+				"		        	<img src=\"https://www.centrodemocratico.org/wp-content/uploads/2018/01/NUEVO-LOGO-CD.png\">\r\n" + 
 				"		        \r\n" + 
-				"		        	<p>Ha sido registrado como voluntario para las elecciones Nacionales en "+evento.getDescripcion()+". </p>\r\n" + 
+				"		        	<p>Ha sido registrado como voluntario para el evento "+evento.getNombre()+". </p>\r\n" + 
+				"		        	<p>Queremos darle las gracias por apoyar nuestro partido y participar en nuestros eventos, esperamos que lo disfrute y comparta con sus amigos.</p>" +
 				"		        	<p>Nos pondremos en contacto con usted en el telefono "+voluntario.getTelefono()+" para darle todos los detalles.</p>";
 		
 		return plantilla;
